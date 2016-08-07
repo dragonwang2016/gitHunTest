e15ab26a288c571caeed6c08ae32c2405320159a
大峰 王 <wangdafeng2014@hotmail.com>
2016-08-07 16:14:57 +0800

    ss

--- App3/App.xaml.cs
+++ App3/App.xaml.cs
@@ -24,7 +24,7 @@ namespace App3
     {
         /// <summary>
         /// 初始化单一实例应用程序对象。这是执行的创作代码的第一行，
-        /// 已执行，逻辑上等同于 main() 或 WinMain()。
+        /// 已执行，逻辑上等同于 main() 或 WinMain()。sasd
         /// </summary>
         public App()
         {
